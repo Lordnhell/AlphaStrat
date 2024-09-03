9cbae9ceae446a7a4672e0fa2e09f0db471e13ac
Lordnhell <87353930+Lordnhell@users.noreply.github.com>
2024-09-03 23:11:07 +0800

    completed gitignore and finalised directory

---
+++ include/OrderGateway.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 3/9/2024.
+//
+
+#ifndef ORDERGATEWAY_H
+#define ORDERGATEWAY_H
+
+#endif //ORDERGATEWAY_H
---
+++ include/TradingEngine.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 3/9/2024.
+//
+
+#ifndef TRADINGENGINE_H
+#define TRADINGENGINE_H
+
+#endif //TRADINGENGINE_H
