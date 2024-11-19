e453b7372ec535b8e81bea9113c4ba28cabe7b6c
Lordnhell <87353930+Lordnhell@users.noreply.github.com>
2024-11-19 11:24:15 +0800

    soft commit

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include "Adapters/AlpacaAdapter.h"
 #include "orderGateway/Order.h"
 #include "interface/AdapterEnum.h"
+#include "../src/MarketDataGateway/EventListener.h"
 
 using namespace std;
 
@@ -37,22 +38,81 @@ void runAdapter(const std::string& configFile, const std::vector<std::string>& t
 
 int main() {
 
-    OrderManagementSystem oms;
-    oms.initializeAdapters();
-    Order example_order("AlpacaOrder001",
-        "AAPL",
-        Order::Side::Buy,
-        100,
-        Order::OrderType::Limit,
-        Order::TimeInForce::Day,
-        "NYSE");
+    try {
+        // Create EventListener objects and start listening
+        EventListener liveEventListener("../config/config.json");
+        EventListener historicalEventListener("../config/config.json");
+        EventListener orderEventListener("../config/config.json");
 
-    // Setting optional parameters
-    example_order.setLimitPrice(145.50);
-    // example_order.setClientOrderID("AlpacaOrder001");
-    example_order.setExtendedHours(true);
+        liveEventListener.startListening();
+        historicalEventListener.startListening();
+        orderEventListener.startListening();
 
-    oms.submitOrder(example_order, AdapterEnum::Alpaca);
+        solaceLib solaceClient("../config/config.json");
+
+        // Setup subscriber threads for each topic
+        std::thread liveSubscriberThread([&]() {
+            solaceClient.subscribeToTopic("marketdata/request/live");
+        });
+
+        std::thread historicalSubscriberThread([&]() {
+            solaceClient.subscribeToTopic("marketdata/request/historical");
+        });
+
+        std::thread orderSubscriberThread([&]() {
+            solaceClient.subscribeToTopic("order/request");
+        });
+
+        // Start threads
+        liveSubscriberThread.detach();
+        historicalSubscriberThread.detach();
+        orderSubscriberThread.detach();
+
+        // Loop to continuously publish messages to the subscribed topics
+        int messageCount = 0;
+        while (messageCount < 100) {
+            // Create test messages
+            std::string testLiveMessage = "{\"event\": \"live_data\", \"message\": \"Live data sample " + std::to_string(messageCount) + "\"}";
+            std::string testHistoricalMessage = "{\"event\": \"historical_data\", \"message\": \"Historical data sample " + std::to_string(messageCount) + "\"}";
+            std::string testOrderMessage = "{\"event\": \"order_request\", \"message\": \"Order request sample " + std::to_string(messageCount) + "\"}";
+
+            // Publish the messages
+            solaceClient.publishMessage("marketdata/request/live", testLiveMessage);
+            solaceClient.publishMessage("marketdata/request/historical", testHistoricalMessage);
+            solaceClient.publishMessage("order/request", testOrderMessage);
+
+            std::cout << "Published messages with count: " << messageCount << std::endl;
+
+            // Wait for a short period before sending the next set of messages
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            messageCount++;
+        }
+
+        // Keep the program running to receive messages (subscriber)
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Exception occurred: " << e.what() << std::endl;
+    }
+
+    // OrderManagementSystem oms;
+    // oms.initializeAdapters();
+    // Order example_order("AlpacaOrder001",
+    //     "AAPL",
+    //     Order::Side::Buy,
+    //     100,
+    //     Order::OrderType::Limit,
+    //     Order::TimeInForce::Day,
+    //     "NYSE");
+    //
+    // // Setting optional parameters
+    // example_order.setLimitPrice(145.50);
+    // // example_order.setClientOrderID("AlpacaOrder001");
+    // example_order.setExtendedHours(true);
+    //
+    // oms.submitOrder(example_order, AdapterEnum::Alpaca);
 
     // unsigned int numThreads = std::thread::hardware_concurrency();
     // std::cout << "Number of hardware threads: " << numThreads << std::endl;
