980aa9adf2668027db79db798e308055f6a32e9f
Lordnhell <87353930+Lordnhell@users.noreply.github.com>
2025-03-16 15:42:16 +0800

    soft commit

---
+++ include/Adapters/IBKRAdapter.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 3/3/2025.
+//
+
+#ifndef IBKRADAPTER_H
+#define IBKRADAPTER_H
+
+
+
+class IBKRAdapter {
+
+};
+
+
+
+#endif //IBKRADAPTER_H
---
+++ src/Adapters/IBKRAdapter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 3/3/2025.
+//
+
+#include "IBKRAdapter.h"
